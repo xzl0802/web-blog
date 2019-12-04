4f9ad997a132c01a02a542b6ab5a0aac8763473c
xzl <xzlperfect@163.com>
2019-12-04 15:43:18 +0800

    添加canvas 雨滴背景

--- src/components/client/header/canvas.vue
+++ src/components/client/header/canvas.vue
@@ -1,16 +1,44 @@
 <template>
-    <div class="canvas_div">
-
-    </div>
+  <div class="canvas_div">
+    <canvas
+      id="canvas"
+   
+      style="position: absolute; left: 0px; bottom: 0px;"
+    ></canvas>
+  </div>
 </template>
 <script>
+import { dropCanvas } from "@/utils/drop.js";
 export default {
-     name:"canvasBg"
-}
+  name: "canvasBg",
+  data() {
+    return {
+      can: "",
+      cxt: "",
+      canW: "",
+      canH: "",
+      drops: []
+    };
+  },
+  methods: {
+    initCanvas() {
+     dropCanvas("canvas")
+    }
+  },
+  mounted() {
+    this.initCanvas();
+  }
+};
 </script>
 <style scoped>
-.canvas_div{
- width: 100%;
- height: 100%;   
+.canvas_div {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+#canvas{
+    opacity: 0.25;
+    width: 100%;
+  height: 100%;
 }
 </style>

686f7c5b538f6ee19a532eed5b8a0d2319db95b2
xzl <xzlperfect@163.com>
2019-12-10 16:37:23 +0800

    添加权限管理界面

---
+++ src/utils/permition.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: xzl 
+ * @Date: 2019-12-10 16:21:54 
+ * @Last Modified by: xzl
+ * @Last Modified time: 2019-12-10 16:37:00
+ */
+//路由跳转权限控制
+  import router from 'vue-router';
+  router.beforeEach((to, from, next) => {
+     
+    // to and from are both route objects. must call `next`.
+  })

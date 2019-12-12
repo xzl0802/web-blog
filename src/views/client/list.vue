5b3b8c6be986b047dd7af4fc61e827bfe1b278d4
xzl <xzlperfect@163.com>
2019-12-12 11:04:55 +0800

    页面框架搭建完成

--- src/views/client/list.vue
+++ src/views/client/list.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="listContent">
-    <a-card class="lsitCard" :hoverable=hoverable>
+    <a-card class="lsitCard" :hoverable="hoverable">
+      <div class="articleContent">
+        <div class="articleItem">
+          <h2>世界，你好！</h2>
+          <a class="articleDesc">开发中</a>
+          <div class="fn__clear">
+            2019-12-11 &nbsp;·&nbsp;
+            <a
+              href="http://www.xzl0802.top:8080/authors/1575957329573"
+            >xzl0802</a>
+            &nbsp;·&nbsp;
+            <a
+              rel="tag"
+              class="item__tag"
+              href="http://www.xzl0802.top:8080/tags/%E5%BC%80%E6%BA%90"
+            >开源</a> &nbsp;
+            <a
+              rel="tag"
+              class="item__tag"
+              href="http://www.xzl0802.top:8080/tags/GitHub"
+            >GitHub</a> &nbsp;
+            &nbsp;·&nbsp;
+            <a
+              class="item__tag"
+              href="http://www.xzl0802.top:8080/my-github-repos"
+            >1 浏览</a>
+          </div>
+        </div>
+      </div>
+
       <div class="pageContent">
-        <a-pagination :defaultCurrent=defaultCurrent :total=total  class="paginationDiv" />
+        <a-pagination :defaultCurrent="defaultCurrent" :total="total" class="paginationDiv" />
       </div>
     </a-card>
   </div>
@@ -13,7 +42,7 @@ export default {
     return {
       defaultCurrent: 1,
       total: 10,
-      hoverable:true
+      hoverable: true
     };
   }
 };
@@ -25,20 +54,48 @@ export default {
   margin: 0 auto;
 }
 .lsitCard {
-  min-height:400px;
+  min-height: 260px;
   margin-top: 30px;
   margin-bottom: -50px;
-
 }
 .pageContent {
-  border-top:1px solid #f2f2f2;
-  position: absolute;
-  bottom: 40px;
- text-align: center;
+  height: 100px;
+  text-align: center;
   width: 100%;
 }
-.paginationDiv{
-margin-top: 40px;
-display: inline-block;
+.paginationDiv {
+  margin-top: 40px;
+  display: inline-block;
+}
+.articleContent {
+  width: 100%;
+      margin-top: -20px;
+}
+.articleItem {
+    margin-top: 30px;
+  height: 120px;
+  border-bottom: 1px solid #f2f2f2;
+}
+
+.articleItem h2 {
+  color: #15171a;
+  font-size: 24px;
+  line-height: 20px;
+  font-weight: 900;
+}
+.articleDesc {
+  display: block;
+  line-height: 24px;
+  color: #15171a;
+  font-size: 15px;
+  margin: 10px 0;
+}
+.articleItem .item__tag {
+  display: inline-block;
+  color: #738a94;
+  line-height: 14px;
+  font-weight: 500;
+  letter-spacing: 0.5px;
+  text-transform: uppercase;
 }
 </style>

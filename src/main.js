2b33436e34aa2854f9c7ae54dd04170954fb2e35
xzl <xzlperfect@163.com>
2019-12-18 15:51:42 +0800

    login

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import 'ant-design-vue/dist/antd.css';
 // import animated from 'animate.css'
 import router from  '@/router'
 import "@/utils/permition"
+import VueParticles from 'vue-particles'
+
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
-
+Vue.use(VueParticles)
 new Vue({
   render: h => h(App),
   router
--- src/views/admin/login/login.vue
+++ src/views/admin/login/login.vue
@@ -1,28 +1,65 @@
 <template>
-<div class="login_main">
-<div class="login_content">
-
-</div>
-    </div>    
+  <div class="login_main">
+ <vue-particles
+        color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+  </div>
 </template>
+
 <script>
 export default {
-    data(){
-        return{
-
-        }
+  data() {
+    return {
+      formLayout: "horizontal"
+    };
+  },
+  computed: {
+    formItemLayout() {
+      const { formLayout } = this;
+      return formLayout === "horizontal"
+        ? {
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 }
+          }
+        : {};
     },
-    methods:{
-
+    buttonItemLayout() {
+      const { formLayout } = this;
+      return formLayout === "horizontal"
+        ? {
+            wrapperCol: { span: 14, offset: 4 }
+          }
+        : {};
     }
-}
+  },
+  methods: {
+    handleFormLayoutChange(e) {
+      this.formLayout = e.target.value;
+    }
+  }
+};
 </script>
 <style scoped>
 .login_main{
     width: 100%;
     height: 100%;
     overflow: hidden;
-    background-color: #f2f2f2;
+    background-color: rgb(79, 79, 80);
     display: flex;
     display: -webkit-flex;
     box-sizing: border-box;
@@ -30,8 +67,6 @@ export default {
 .login_content{
     width:450px;
     height: 500px;
-    box-shadow: 5px 2px 5px  #ffffff;
-    background: #ffffff;
     margin: auto;
 }
 </style>

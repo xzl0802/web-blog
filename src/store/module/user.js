7818d1abb23b74307a73b6554cc0340a4bf00c71
xzl <xzlperfect@163.com>
2019-12-20 14:55:32 +0800

    配置状态管理

---
+++ src/store/module/user.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: xzl 
+ * @Date: 2019-12-20 14:38:24 
+ * @Last Modified by: xzl
+ * @Last Modified time: 2019-12-20 14:51:59
+ */
+
+ import Vue from  "vue";
+ import Vuex from 'vuex';
+
+ const store  = new Vuex.Store({
+     
+ })
+
+ export default store;
--- src/vue.config.js
+++ src/vue.config.js
@@ -2,11 +2,12 @@
  * @Author: xzl 
  * @Date: 2019-12-12 13:57:34 
  * @Last Modified by: xzl
- * @Last Modified time: 2019-12-13 10:17:47
+ * @Last Modified time: 2019-12-20 14:29:24
  */
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 const productionGzipExtensions = ['js', 'css'];
 module.exports = {
+  productionSourceMap: false,
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {    //添加gzip 压缩
             config.plugins.push(new CompressionWebpackPlugin({

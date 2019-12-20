69a207c1ee0f51207beee10480b18500a3a583ed
xzl <xzlperfect@163.com>
2019-12-20 13:25:56 +0800

    删除粒子特效

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ import 'ant-design-vue/dist/antd.css';
 // import animated from 'animate.css'
 import router from  '@/router'
 import "@/utils/permition"
-import VueParticles from 'vue-particles'
+
 
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
-Vue.use(VueParticles)
 new Vue({
   render: h => h(App),
   router
--- src/views/admin/login/login.vue
+++ src/views/admin/login/login.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="login_main">
- <vue-particles
-        color="#dedede"
-        :particleOpacity="0.7"
-        :particlesNumber="80"
-        shapeType="circle"
-        :particleSize="4"
-        linesColor="#dedede"
-        :linesWidth="1"
-        :lineLinked="true"
-        :lineOpacity="0.4"
-        :linesDistance="150"
-        :moveSpeed="3"
-        :hoverEffect="true"
-        hoverMode="grab"
-        :clickEffect="true"
-        clickMode="push"
-      >
-      </vue-particles>
+ 
   </div>
 </template>
 
@@ -25,32 +8,14 @@
 export default {
   data() {
     return {
-      formLayout: "horizontal"
+  
     };
   },
   computed: {
-    formItemLayout() {
-      const { formLayout } = this;
-      return formLayout === "horizontal"
-        ? {
-            labelCol: { span: 4 },
-            wrapperCol: { span: 14 }
-          }
-        : {};
-    },
-    buttonItemLayout() {
-      const { formLayout } = this;
-      return formLayout === "horizontal"
-        ? {
-            wrapperCol: { span: 14, offset: 4 }
-          }
-        : {};
-    }
+ 
   },
   methods: {
-    handleFormLayoutChange(e) {
-      this.formLayout = e.target.value;
-    }
+  
   }
 };
 </script>
@@ -59,7 +24,7 @@ export default {
     width: 100%;
     height: 100%;
     overflow: hidden;
-    background-color: rgb(79, 79, 80);
+
     display: flex;
     display: -webkit-flex;
     box-sizing: border-box;
